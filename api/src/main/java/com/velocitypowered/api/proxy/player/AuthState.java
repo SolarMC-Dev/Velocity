f3b8971aa3d38d13e7873af289a8d378337dafbe
A248 <anandebeh@gmail.com>
2021-04-01 13:13:11 -0400

    Fix whatever oddity happened to AuthState

--- api/src/main/java/com/velocitypowered/api/proxy/player/AuthState.java
+++ api/src/main/java/com/velocitypowered/api/proxy/player/AuthState.java
@@ -1,12 +1,15 @@
-Unstaged changes after reset:
-M	api/src/main/java/com/velocitypowered/api/proxy/InboundConnection.java
-M	api/src/main/java/com/velocitypowered/api/proxy/Player.java
-M	api/src/main/java/com/velocitypowered/api/proxy/player/AuthState.java
-D	api/src/main/java/com/velocitypowered/api/proxy/player/Authenticatable.java
-M	proxy/src/main/java/com/velocitypowered/proxy/Velocity.java
-M	proxy/src/main/java/com/velocitypowered/proxy/VelocityServer.java
-M	proxy/src/main/java/com/velocitypowered/proxy/connection/client/ConnectedPlayer.java
-M	proxy/src/main/java/com/velocitypowered/proxy/connection/client/HandshakeSessionHandler.java
-M	proxy/src/main/java/com/velocitypowered/proxy/connection/client/InitialInboundConnection.java
-M	proxy/src/main/java/com/velocitypowered/proxy/connection/client/LoginSessionHandler.java
-M	proxy/src/test/java/com/velocitypowered/proxy/plugin/VelocityEventManagerTest.java
+package com.velocitypowered.api.proxy.player;
+
+/**
+ * Authentication information for a player, used by the authentication provider
+ *
+ */
+public interface AuthState {
+
+  /**
+   * Determines whether the player is authenticated
+   *
+   * @return true if authenticated and ready to play, false otherwise
+   */
+  boolean isAuthenticated();
+}
